97754ecbe9fe7d07a52604a42924b454723da007
Jacob Hampton <jacob_hampton@uri.edu>
2021-04-19 15:34:21 -0700

    its linkin

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-set -e
 progDir='/home/jacobhampton/Prog06/Prog06/'
 exeDir="${progDir}Executable"
 if [[ ! -d "$exeDir" ]]
@@ -10,13 +8,25 @@ fi
 for i in 1 2 3
 do
     verDir="${progDir}Version$i"
+    d="${verDir##*/}"
+    ex="${d/V/v}"
+    final=""
     for file in $verDir/*.cpp
     do
         f="${file##*/}"
-        ex="${f%.cpp}"
-        g++ -std=c++14 -Wall $file -lstdc++ -lpthread -o $ex
-        mv $ex "$exeDir"
+        op="${f%.cpp}"
+        if [[ $op != $ex ]]
+        then
+            f="${file##*/}"
+            op="${f%.cpp}"
+            g++ -std=c++14 -Wall -lstdc++ -lpthread -c $file 
+        else
+            final=$file
+        fi
     done
+    g++ -std=c++14 -Wall $final -lstdc++ -lpthread -o $ex *.o
+    mv $ex "$exeDir"
+    rm -f *.o
     ##mv $ex "${progDir%/*}"
     ##if (($count == $vr)); then
     ##    ./$ex "/home/jacobhampton/Prog05/Data/smallMap_Horiz.map" "sajhbd" 1 1 1
