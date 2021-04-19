3af71e69a01fb4ba4d64e14551f141cc699b56c9
Jacob Hampton <jacob_hampton@uri.edu>
2021-04-19 09:23:19 -0700

    this is dumb

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-progDir='/home/jacobhampton/Prog05/Programs/*.cpp'
-vr=$1
-count=0
-for file in $progDir
+progDir='/home/jacobhampton/Prog06/Prog06/'
+exeDir="${progDir}Executable"
+#echo `mkdir ${exeDir}`
+if [[-d "$exeDir"]];
+then
+    echo worked
+fi
+#vr=$1
+#count=0
+for i in 1 2 3
 do
-    count=$((count+1))
-    f="${file##*/}"
-    ex="${f%.cpp}"
-    g++ -std=c++14 -Wall $file -lstdc++ -lpthread -o $ex
-    mv $ex "${progDir%/*}"
-    if (($count == $vr)); then
-        ./$ex "/home/jacobhampton/Prog05/Data/smallMap_Horiz.map" "sajhbd" 1 1 1
-    fi 
+    verDir="${progDir}Version$i"
+    for file in $verDir/*.cpp
+    do
+        echo $file
+        ##f="${file##*/}"
+        ##ex="${f%.cpp}"
+        ##g++ -std=c++14 -Wall $file -lstdc++ -lpthread -o $ex
+    done
+    ##mv $ex "${progDir%/*}"
+    ##if (($count == $vr)); then
+    ##    ./$ex "/home/jacobhampton/Prog05/Data/smallMap_Horiz.map" "sajhbd" 1 1 1
+    ##fi 
 done
